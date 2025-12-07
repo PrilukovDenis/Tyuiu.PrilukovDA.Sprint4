d570789e294b3b198df98ff7d6f6412ff528efbd
deusvela <d.prilukov@aerokod.ru>
2025-12-07 15:08:18 +0500

    Task5

---
+++ Tyuiu.PrilukovDA.Sprint4.Task5.V17.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.PrilukovDA.Sprint4.Task5.V17.Lib;
+
+namespace Tyuiu.PrilukovDA.Sprint4.Task5.V17.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int[,] matrix = new int[5, 5]
+       {
+            { -5, -9, 8, 0, -2 },
+            { -4, -9, -8, 3, 1 },
+            { 2, 5, 4, -3, -1 },
+            { -6, 0, -7, 4, -8 },
+            { 1, -2, 3, -4, 2 }
+       };
+
+            Assert.AreEqual(13, ds.Calculate(matrix));
+        }
+    }
+}
---
+++ Tyuiu.PrilukovDA.Sprint4.Task5.V17/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using Tyuiu.PrilukovDA.Sprint4.Task5.V17.Lib;
+
+DataService ds = new DataService();
+
+int[,] matrix = new int[5, 5]
+       {
+            { -5, -9, 8, 0, -2 },
+            { -4, -9, -8, 3, 1 },
+            { 2, 5, 4, -3, -1 },
+            { -6, 0, -7, 4, -8 },
+            { 1, -2, 3, -4, 2 }
+       };
+
+Console.WriteLine("Матрица 5x5:");
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        Console.Write(matrix[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+
+int result = ds.Calculate(matrix);
+Console.WriteLine("\nКоличество отрицательных элементов: " + result);
+Console.ReadKey();
+
