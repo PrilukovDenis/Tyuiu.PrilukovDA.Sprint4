0d400402f7cc53d16cc0e03e7c0632c90d96f493
deusvela <d.prilukov@aerokod.ru>
2025-12-07 14:43:44 +0500

    Task4

---
+++ Tyuiu.PrilukovDA.Sprint4.Task4.V24.Test/DataServiceTest.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.ApplicationInsights;
+using Tyuiu.PrilukovDA.Sprint4.Task4.V24.Lib;
+
+namespace Tyuiu.PrilukovDA.Sprint4.Task4.V24.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int[,] matrix = new int[5, 5]
+{
+            { 7, 9, 7, 6, 7 },
+            { 9, 9, 8, 6, 7 },
+            { 8, 6, 5, 6, 7 },
+            { 9, 9, 7, 8, 7 },
+            { 5, 9, 9, 8, 9 }
+};
+
+            int[,] result = ds.Calculate(matrix);
+
+            int rows = result.GetLength(0);
+            int cols = result.GetLength(1);
+
+            int[,] truthArr = {
+                { 7, 9, 7, 1, 7 },
+                { 9, 9, 1, 1, 7 },
+                { 1, 1, 5, 1, 7 },
+                { 9, 9, 7, 1, 7 },
+                { 5, 9, 9, 1, 9 }
+            };
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Assert.AreEqual(truthArr[i, j], result[i, j]);
+                }
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
---
+++ Tyuiu.PrilukovDA.Sprint4.Task4.V24/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.PrilukovDA.Sprint4.Task4.V24.Lib;
+
+DataService ds = new DataService();
+
+int[,] matrix = new int[5, 5]
+{
+            { 7, 9, 7, 6, 7 },
+            { 9, 9, 8, 6, 7 },
+            { 8, 6, 5, 6, 7 },
+            { 9, 9, 7, 8, 7 },
+            { 5, 9, 9, 8, 9 }
+};
+
+Console.WriteLine("Исходный массив 5x5:");
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        Console.Write(matrix[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Исправленный массив 5x5:");
+
+int[,] result = ds.Calculate(matrix);
+
+int rows = result.GetLength(0);
+int cols = result.GetLength(1);
+
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < cols; j++)
+    {
+        Console.Write(result[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.ReadKey();
